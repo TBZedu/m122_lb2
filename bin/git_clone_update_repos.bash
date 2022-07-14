36025053628af75fba080b80379fb57f56d8028b
Dillan <d@illan.ch>
2022-07-14 14:48:52 +0200

    feat(git_clone_update_repos): works now

--- bin/git_clone_update_repos.bash
+++ bin/git_clone_update_repos.bash
@@ -3,7 +3,7 @@
 cwd=`pwd`
 cd `dirname $0`	
 BINDIR=`pwd`
-LOGLEVEL=${LOGLEVEL:-D}
+LOGLEVEL=${LOGLEVEL:-I}
 cd $cwd
 BASENAME=`basename $0`
 LOGFILE=$BINDIR/../var/buc.log
@@ -12,14 +12,14 @@ LOGFILE=$BINDIR/../var/git-tools.log
 
 usage() {
   echo 'SYNOPSIS:'                                                              >&2
-  echo "    $BASENAME <INPUT_FILE> [-b <BASE_DIRECTORY>] [<ARGUMENTS>]"         >&2
+  echo "    $BASENAME [-b <BASE_DIRECTORY>] [<ARGUMENTS>] <INPUT_FILE>"         >&2
   echo ''                                                                       >&2
   echo 'INPUT_FILE:'                                                            >&2
   echo '    A file containing all git repo urls and folder names.'              >&2
   echo ''                                                                       >&2
   echo 'ARGUMENTS:'                                                             >&2
   echo '    -b BASE_DIRECTORY         A directory containing git repositories.' >&2
-  echo '    -v[=LEVEL]                   log verbosity (D|I|W|E), default is D' >&2
+  echo '    -v LEVEL                     log verbosity (D|I|W|E), default is D' >&2
   echo ''                                                                       >&2
   echo 'EXIT CODES:'                                                            >&2
   echo '    0   Success'                                                        >&2
@@ -33,23 +33,29 @@ usage() {
 # Base directory should be the current directory by default.
 base_directory=$cwd
 
-while getopts b:h: optvar; do
+while getopts b:v: optvar; do
 	case $optvar in
-		p) base_directory=${OPTARG} ;;
+		b) base_directory=${OPTARG} ;;
 		v)
       if [[ $2 == "D" || $2 == "I" || $2 == "W" || $2 == "E" ]]; then
         LOGLEVEL=$2
       else
         LOGLEVEL=D
       fi
-    ;;
+      ;;
+    :)
+      log E "$0: Please provide an argument to -$OPTARG."
+      exit 1
+      ;;
 		*) usage && exit 1 ;;
 	esac
 done
+shift $((OPTIND-1))
 
-input_dir="$1"
+input_file="$1"
 # We can't continue if the input dir is not a file
-if [ ! -f "$input_dir" ]; then
+log D "Using input file $input_file"
+if [ ! -f "$input_file" ]; then
   usage && exit 1
 fi
 
@@ -59,17 +65,25 @@ if [ ! -d "$base_directory" ]; then
   exit 1
 fi
 
-cat $input_dir | while read git_url folder_name ; do
+log I "Using $base_directory as a bsae directory"
+
+cat $input_file | while read git_url folder_name ; do
 
   if [ -d $base_directory/$folder_name ]; then
     log I "Repository $folder_name exists already. Pulling..."
     cd $base_directory/$folder_name && git pull
   else
-    log I "Cloning $folder_name..."
+    log I "Cloning $folder_name as $folder_name..."
     git clone $git_url $base_directory/$folder_name
   fi
 
 done
 
-# TODO: Delete folders in base directory that are not a git repository
+for folder in `ls $base_directory`; do
 
+  # We want to delete folder that are not within our input file. The 2 spaces are intentional!
+  if [[ -d $base_directory/$folder && -z "$(grep  $folder $input_file)" ]]; then
+    log I "The folder $folder is not contained within the input file. Deleting $base_directory/$folder..."
+    rm -rf $base_directory/$folder
+  fi
+done
